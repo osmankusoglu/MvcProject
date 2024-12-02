2893ea72c85c285d4e6c03486c587ee2d0fa4563
Osman Kuşoğlu <formatatalim@outlook.com>
2024-12-02 18:51:10 +0300

    New title creating writer and category operations

---
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Web.WebPages.Html;
+using SelectListItem = System.Web.Mvc.SelectListItem;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text=x.CategoryName,
+                                                      Value=x.CategoryID.ToString()
+                                                  }).ToList();
+
+            List<SelectListItem> valuewriter = (from x in wm.GetList()
+                                                select new SelectListItem
+                                                { 
+                                                    Text=x.WriterName + " " + x.WriterSurName,
+                                                    Value=x.WriterID.ToString()
+                                                }).ToList();
+            ViewBag.vlc=valuecategory;
+            ViewBag.vlw=valuewriter;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate=DateTime.Parse(DateTime.Now.ToLongDateString());
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
