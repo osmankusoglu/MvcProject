432b1186ed30fc0373f3760bd5227ef97e34b408
Osman Kuşoğlu <formatatalim@outlook.com>
2024-12-12 17:30:59 +0300

    Message Page

---
+++ MvcProjeKampi/Controllers/MessageController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concreate;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        MessageManager cm = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messageList = cm.GetListInbox();
+            return View(messageList);
+        }
+        public ActionResult Sendbox()
+        {
+            var messageList = cm.GetListSendbox();
+            return View(messageList);
+        }
+
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            return View();
+        }
+    }
+}
